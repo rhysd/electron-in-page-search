69d47c17f7e3c7ba0216f6ada7d513acfcabfa97
rhysd <lin90162@yahoo.co.jp>
2018-04-02 12:00:18 +0900

    separate test helpers to helper.ts

---
+++ test/helper.ts
@@ -0,0 +1,45 @@
+import { remote } from 'electron';
+
+export function waitForReady(w: Electron.WebviewTag) {
+    return new Promise(resolve => {
+        const c = w.getWebContents && w.getWebContents();
+        if (c) {
+            resolve(w);
+            return;
+        }
+        w.addEventListener('dom-ready', resolve);
+    });
+}
+
+export function pause(msec: number) {
+    return () =>
+        new Promise(resolve => {
+            setTimeout(resolve, msec);
+        });
+}
+
+export function clickButton(w: Electron.WebviewTag, button: 'forward' | 'back' | 'close') {
+    return () =>
+        new Promise(resolve => {
+            const sel = `.inpage-search-${button}`;
+            const src = `(function(){ document.querySelector('${sel}').click(); })()`;
+            w.executeJavaScript(src, false, resolve);
+        });
+}
+
+export function searchStart(w: Electron.WebviewTag, query: string, click_times: number, forward: boolean = true) {
+    return () =>
+        new Promise(resolve => {
+            remote.getCurrentWindow().focusOnWebView();
+
+            const selector = forward ? '.inpage-search-forward' : '.inpage-search-back';
+            let src = '(function() {';
+            src += `document.querySelector(".inpage-search-input").value = "${query}";`;
+            src += `const b = document.querySelector('${selector}');`;
+            for (let i = 0; i < click_times; ++i) {
+                src += 'b.click();';
+            }
+            src += '})()';
+            w.executeJavaScript(src, false, resolve);
+        });
+}
--- test/smoke_browser_window_test.ts
+++ test/smoke_browser_window_test.ts
@@ -2,23 +2,7 @@ import searchInPage from '../src/index';
 import * as A from 'assert';
 import { remote } from 'electron';
 import { spy } from 'sinon';
-
-function waitForReady(w: Electron.WebviewTag) {
-    return new Promise(resolve => {
-        const c = w.getWebContents && w.getWebContents();
-        if (c) {
-            resolve(w);
-            return;
-        }
-        w.addEventListener('dom-ready', resolve);
-    });
-}
-
-function pause1000ms() {
-    return new Promise(resolve => {
-        setTimeout(resolve, 1000);
-    });
-}
+import { waitForReady, pause, searchStart, clickButton } from './helper';
 
 context('For browser window', function() {
     before(function() {
@@ -51,45 +35,25 @@ context('For browser window', function() {
 
             const next = spy();
             return waitForReady(w)
-                .then(pause1000ms)
-                .then(() => {
-                    remote.getCurrentWindow().focusOnWebView();
-                    w.executeJavaScript(
-                        `(function() {
-                            document.querySelector('.inpage-search-input').value = 'foo';
-                            document.querySelector('.inpage-search-forward').click();
-                        })()`,
-                        false,
-                    );
-                })
-                .then(pause1000ms)
+                .then(pause(1000))
+                .then(searchStart(w, 'foo', 1))
+                .then(pause(1000))
                 .then(() => {
                     A.ok(s.isSearching());
                     A.ok(started.called);
                     A.equal(started.args[0][0], 'foo');
 
                     s.on('next', next);
-
-                    w.executeJavaScript(
-                        `(function() {
-                            document.querySelector('.inpage-search-forward').click();
-                        })()`,
-                        false,
-                    );
                 })
-                .then(pause1000ms)
+                .then(clickButton(w, 'forward'))
+                .then(pause(1000))
                 .then(() => {
                     A.ok(next.called);
                     A.equal(next.args[0][0], 'foo');
                     A.ok(next.args[0][1]);
-                    w.executeJavaScript(
-                        `(function() {
-                            document.querySelector('.inpage-search-close').click();
-                        })()`,
-                        false,
-                    );
                 })
-                .then(pause1000ms)
+                .then(clickButton(w, 'close'))
+                .then(pause(1000))
                 .then(() => {
                     A.ok(!s.opened);
                     A.ok(stopped.called);
@@ -108,33 +72,11 @@ context('For browser window', function() {
             s.on('next', next);
             s.on('start', start);
             return waitForReady(w)
-                .then(pause1000ms)
-                .then(() => {
-                    remote.getCurrentWindow().focusOnWebView();
-                    w.executeJavaScript(
-                        `(function() {
-                            document.querySelector('.inpage-search-input').value = 'foo';
-                            const b = document.querySelector('.inpage-search-forward');
-                            b.click();
-                            b.click();
-                        })()`,
-                        false,
-                    );
-                })
-                .then(pause1000ms)
-                .then(() => {
-                    remote.getCurrentWindow().focusOnWebView();
-                    w.executeJavaScript(
-                        `(function() {
-                            document.querySelector('.inpage-search-input').value = 'ba';
-                            const b = document.querySelector('.inpage-search-forward');
-                            b.click();
-                            b.click();
-                        })()`,
-                        false,
-                    );
-                })
-                .then(pause1000ms)
+                .then(pause(1000))
+                .then(searchStart(w, 'foo', 2))
+                .then(pause(1000))
+                .then(searchStart(w, 'ba', 2))
+                .then(pause(1000))
                 .then(() => {
                     A.equal(start.args[0][0], 'foo');
                     A.equal(start.args[1][0], 'ba');
@@ -143,15 +85,8 @@ context('For browser window', function() {
                     A.equal(next.args[1][0], 'ba');
                     A.ok(next.args[1][1]);
                 })
-                .then(() => {
-                    w.executeJavaScript(
-                        `(function() {
-                            document.querySelector('.inpage-search-close').click();
-                        })()`,
-                        false,
-                    );
-                })
-                .then(pause1000ms)
+                .then(clickButton(w, 'close'))
+                .then(pause(1000))
                 .then(() => {
                     A.ok(!s.isSearching());
                     A.ok(!s.opened);
@@ -169,56 +104,28 @@ context('For browser window', function() {
             const start = spy();
             const stop = spy();
             return waitForReady(w)
-                .then(pause1000ms)
-                .then(() => {
-                    remote.getCurrentWindow().focusOnWebView();
-                    w.executeJavaScript(
-                        `(function() {
-                            document.querySelector('.inpage-search-input').value = 'foo';
-                            const b = document.querySelector('.inpage-search-forward');
-                            b.click();
-                            b.click();
-                        })()`,
-                        false,
-                    );
-                })
-                .then(pause1000ms)
+                .then(pause(1000))
+                .then(searchStart(w, 'foo', 2))
+                .then(pause(1000))
                 .then(() => s.closeSearchWindow())
-                .then(pause1000ms)
+                .then(pause(1000))
                 .then(() => {
                     A.equal(w.className, 'electron-in-page-search-window search-inactive');
                     s.on('next', next);
                     s.on('start', start);
                     s.openSearchWindow();
                 })
-                .then(pause1000ms)
-                .then(() => {
-                    remote.getCurrentWindow().focusOnWebView();
-                    w.executeJavaScript(
-                        `(function() {
-                            document.querySelector('.inpage-search-input').value = 'ba';
-                            const b = document.querySelector('.inpage-search-forward');
-                            b.click();
-                            b.click();
-                        })()`,
-                        false,
-                    );
-                })
-                .then(pause1000ms)
+                .then(pause(1000))
+                .then(searchStart(w, 'ba', 2))
+                .then(pause(1000))
                 .then(() => {
                     A.equal(start.args[0][0], 'ba');
                     A.equal(next.args[0][0], 'ba');
                     A.ok(next.args[0][1]);
                     s.on('stop', stop);
-
-                    w.executeJavaScript(
-                        `(function() {
-                            document.querySelector('.inpage-search-close').click();
-                        })()`,
-                        false,
-                    );
                 })
-                .then(pause1000ms)
+                .then(clickButton(w, 'close'))
+                .then(pause(1000))
                 .then(() => {
                     A.ok(stop.called);
                     A.ok(!s.opened);
