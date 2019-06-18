399dc7cc90b4816c2a1aafacea72edf0b831dc33
rhysd <lin90162@yahoo.co.jp>
2019-06-18 22:14:04 +0900

    update dependencies

--- example/webview/main.js
+++ example/webview/main.js
@@ -3,13 +3,22 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const join = require('path').join;
 
-app.once('window-all-closed',function() { app.quit(); });
+app.once('window-all-closed', function() {
+    app.quit();
+});
 
 app.once('ready', function() {
-    let w = new BrowserWindow();
-    w.once('closed', function() { w = null; });
+    let w = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            webviewTag: true,
+        },
+    });
+    w.once('closed', function() {
+        w = null;
+    });
     w.loadURL('file://' + join(__dirname, 'index.html'));
     if (process.env.ELECTRON_IN_PAGE_SEARCH_DEBUG) {
-        w.webContents.openDevTools({mode: 'detach'});
+        w.webContents.openDevTools({ mode: 'detach' });
     }
 });
